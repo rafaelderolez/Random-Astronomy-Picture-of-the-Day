a0369bef7c982e6db09bb348703b34fcab74ba94
Rafael <rafael@neverbland.com>
2016-03-10 19:54:58 +0000

    Refactoring

--- javascript/views/home.js
+++ javascript/views/home.js
@@ -4,13 +4,17 @@ App.Views.Home = Backbone.View.extend({
     model: App.Models.PhotoModel,
 
     events: {
-        "click .new-apod": "getNewApod",
+        'click .new-apod': 'onNewApodBtClick',
+        'click .expand': 'onExpandBtClick'
     },
+
     /**
      * View init
      */
     initialize: function() {
         var _this = this;
+
+        this.isDescriptionVisible = false;
 
         this.album = new App.Collections.PhotosCollection();
         this.album.fetch().done(this.render.bind(this));
@@ -20,14 +24,26 @@ App.Views.Home = Backbone.View.extend({
     },
 
     render: function() {
+        var $tempEl = $(document.createDocumentFragment());
+
         this.getOneApod();
-        this.wrapMatrixCharacters();
-        setTimeout(_.bind(this.matrixFade, this), 1000);
+
+        var view = new App.Views.PhotoItem({
+            model: this.randomEl,
+            options: {
+                descriptionVisible: this.isDescriptionVisible
+            }
+        });
+
+        $tempEl.append(view.render().el);
+
+        this.$('.main-container').append($tempEl);
+
+        view.viewDidRender();
     },
 
     getOneApod: function() {
         this.randomEl = _.sample(this.album.models);
-        this.renderRandomApod();
     },
 
     getNewApod: function() {
@@ -42,63 +58,17 @@ App.Views.Home = Backbone.View.extend({
 
         setTimeout(function() {
             _this.apod.remove();
-            _this.newRandomEl = _.sample(_this.album.models);
-
-            var $tempElTwo = $(document.createDocumentFragment());
-
-            var newView = new App.Views.PhotoItem({
-                model: _this.newRandomEl
-            });
-
-            $tempElTwo.append(newView.render().el);
-
-            _this.$('.main-container').append($tempElTwo);
-            _this.wrapMatrixCharacters();
-            _this.matrixFade();
+            _this.render();
         }, 400);
 
-
-
         return this;
     },
 
-    renderRandomApod: function() {
-        var $tempEl = $(document.createDocumentFragment());
-
-        var view = new App.Views.PhotoItem({
-            model: this.randomEl
-        });
-
-        $tempEl.append(view.render().el);
-
-        this.$('.main-container').append($tempEl);
-
-        return this;
+    onNewApodBtClick: function() {
+        this.getNewApod();
     },
-
-    wrapMatrixCharacters: function() {
-        var $glitch = $('.glitch');
-
-        $glitch.each(function(index) {
-            var $this = $(this);
-            var character = $(this).text().split('');
-
-            $this.empty();
-            $.each(character, function(i, v) {
-                $this.append($('<span>').text(v));
-            });
-        });
+    onExpandBtClick: function() {
+        this.isDescriptionVisible = this.isDescriptionVisible ? false : true;
     },
-    matrixFade: function() {
-        var $glitch = $('.glitch');
-        var $glitchCharacter = $glitch.find('span');
-        var interval = setInterval(function() {
-            var $ds = $glitchCharacter.not(".active");
-            $ds.eq(Math.floor(Math.random() * $ds.length)).addClass('active');
-            if ($ds.length == 1) {
-                clearInterval(interval);
-            }
-        }, 5);
-    }
 
 });
--- javascript/views/photo-item.js
+++ javascript/views/photo-item.js
@@ -6,6 +6,10 @@ App.Views.PhotoItem = Backbone.View.extend({
 
     templateEl: $('#tplApod'),
 
+    events: {
+        'click .expand': 'expandApodDescription'
+    },
+
     template: function(attributes) {
         return _.template(this.templateEl.html())(attributes);
     },
@@ -13,13 +17,62 @@ App.Views.PhotoItem = Backbone.View.extend({
     /**
      * View init
      */
-    initialize: function() {
+    initialize: function(datas) {
+        this.options = datas.options;
+        console.log(this.options)
+    },
 
+    expandApodDescription: function(ev) {
+        var $expand = $(ev.currentTarget);
+        var $readMore = $('.apod__read-more');
+        $expand.toggleClass('expand--active');
+
+        if ($readMore.is(':visible')) {
+            $readMore.fadeOut('slow');
+        } else {
+            $readMore.fadeIn('slow');
+        }
+
+    },
+
+    wrapMatrixCharacters: function() {
+        var $glitch = $('.glitch');
+
+        $glitch.each(function(index) {
+            var $this = $(this);
+            var character = $(this).text().split('');
+
+            $this.empty();
+            $.each(character, function(i, v) {
+                $this.append($('<span>').text(v));
+            });
+        });
+    },
+
+    matrixFade: function() {
+        var $glitch = $('.glitch');
+        var $glitchCharacter = $glitch.find('span');
+        var interval = setInterval(function() {
+            var $ds = $glitchCharacter.not('.active');
+            $ds.eq(Math.floor(Math.random() * $ds.length)).addClass('active');
+            if ($ds.length == 1) {
+                clearInterval(interval);
+            }
+        }, 5);
     },
 
     render: function() {
-        this.$el.html(this.template(this.model.attributes));
+        this.$el.html(this.template({
+            model: this.model.attributes,
+            options: this.options
+        }));
+
         return this;
     },
+
+    viewDidRender: function() {
+        this.wrapMatrixCharacters();
+        this.matrixFade();
+    }
 
 });
